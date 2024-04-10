52b837b05723e3365d6f41898cdb0967823b4703
JasonYang <yzsenmonster@gmail.com>
2024-04-10 15:21:14 +0800

    switch logo svg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,43 +1,39 @@
-'use client';
-import type { Metadata } from 'next';
+"use client";
+import type { Metadata } from "next";
 
-import { ColorSchemeScript, MantineProvider } from '@mantine/core';
-import { AppProvider } from './provider';
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import { AppProvider } from "./provider";
 
-import '@mantine/core/styles.css'; // mantine css style
-import './globals.css'; // tailwind css style
-import './react-toastify.css';
+import "@mantine/core/styles.css"; // mantine css style
+import "./globals.css"; // tailwind css style
+import "./react-toastify.css";
 
-import { ToastContainer } from 'react-toastify';
-import { RecoilRoot } from 'recoil';
+import { ToastContainer } from "react-toastify";
+import { RecoilRoot } from "recoil";
 
 // export const metadata: Metadata = {
 //   title: 'MetaID',
 //   description: 'Cross-Chain DID Protocol Born for Web3',
 // };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang='en'>
-      <head>
-        <ColorSchemeScript />
-        <link rel='shortcut icon' href='/favicon.svg' />
-        <meta
-          name='viewport'
-          content='minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no'
-        />
-      </head>
-      <body>
-        <RecoilRoot>
-          <MantineProvider>
-            <AppProvider>{children}</AppProvider>
-          </MantineProvider>
-        </RecoilRoot>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang="en">
+			<head>
+				<ColorSchemeScript />
+				<link rel="shortcut icon" href="/logo_small.svg" />
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+				/>
+			</head>
+			<body>
+				<RecoilRoot>
+					<MantineProvider>
+						<AppProvider>{children}</AppProvider>
+					</MantineProvider>
+				</RecoilRoot>
+			</body>
+		</html>
+	);
 }
