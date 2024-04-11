caf65b3c66c6ff585a0ad00ebc31d327c16b20e9
JasonYang <yzsenmonster@gmail.com>
2024-04-11 14:28:19 +0800

    add queryClient stale time infinity config

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,35 +1,41 @@
-'use client';
+"use client";
 
-import { useMantineColorScheme } from '@mantine/core';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ToastContainer } from 'react-toastify';
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useMantineColorScheme } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: Infinity,
+		},
+	},
+});
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-  const { colorScheme } = useMantineColorScheme();
+	const { colorScheme } = useMantineColorScheme();
 
-  return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ToastContainer
-        position='top-center'
-        toastStyle={{
-          width: '380px',
-        }}
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme={colorScheme === 'dark' ? 'dark' : 'light'}
-        closeButton={false}
-      />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ToastContainer
+				position="top-center"
+				toastStyle={{
+					width: "380px",
+				}}
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme={colorScheme === "dark" ? "dark" : "light"}
+				closeButton={false}
+			/>
+			<ReactQueryDevtools initialIsOpen={true} />
+		</QueryClientProvider>
+	);
 }
